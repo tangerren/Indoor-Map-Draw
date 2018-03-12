92f1da9212720dae0586402d25a0c02601266166
WoShiSunWuKong <1026435687@qq.com>
2018-03-12 21:46:40 +0800

    new mall And floor

--- src/app/draw-tool/draw-tool.component.ts
+++ src/app/draw-tool/draw-tool.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 
-import { FeatureGroup, Control } from 'leaflet';
+import { FeatureGroup, Control, Draw, DrawOptions } from 'leaflet';
 
 import 'leaflet-draw';
 
@@ -12,23 +12,54 @@ import 'leaflet-draw';
 export class DrawToolComponent implements OnInit {
 
     @Input() map: any;
-    drawnItems: FeatureGroup;
+    drawnItems: FeatureGroup[] = [];
     constructor() { }
 
     ngOnInit() {
-        this.drawnItems = new FeatureGroup();
-        this.map.addLayer(this.drawnItems);
-        this.map.addControl(new Control.Draw({
-            edit: {
-                featureGroup: this.drawnItems
-            }
-        }));
+
+    }
+
+    /**
+     * 每层楼一个FeatureGroup
+     */
+    creat(type) {
+        const floorIndex = this.drawnItems.length;
+        if (floorIndex === 0 && type === 'floor') {
+            console.log('请先创建Mall！');
+            return;
+        }
+        this.drawnItems[floorIndex] = new FeatureGroup();
+        this.map.addLayer(this.drawnItems[floorIndex]);
+        // 清除前几个图层绑定的 draw:created
+        this.clearOnDrawCreated();
         this.map.on('draw:created', (event) => {
-            this.drawnItems.addLayer(event.layer);
+            this.drawnItems[floorIndex].addLayer(event.layer);
         });
     }
 
+    private clearOnDrawCreated() {
+        this.map.off('draw:created');
+    }
+
     log() {
-        console.log(this.map);
+        console.log(this.drawnItems);
+    }
+
+    draw(type) {
+        if (type === 'Rectangle') {
+            const a = new Draw.Rectangle(this.map);
+            a.initialize(this.map, {});
+            a.enable();
+        }
+        if (type === 'Circle') {
+            const a = new Draw.Circle(this.map);
+            a.initialize(this.map, {});
+            a.enable();
+        }
+        if (type === 'Polygon') {
+            const a = new Draw.Polygon(this.map);
+            a.initialize(this.map, {});
+            a.enable();
+        }
     }
 }
