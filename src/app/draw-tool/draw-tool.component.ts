e8489c9ebf707843e1f4bd2d1f31688c52bdfd0b
WoShiSunWuKong <1026435687@qq.com>
2018-03-18 13:41:46 +0800

    save geojson as a json file

--- src/app/draw-tool/draw-tool.component.ts
+++ src/app/draw-tool/draw-tool.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 
-import { Map, interaction, layer, source, style, Feature } from 'openlayers';
+import { Map, interaction, layer, source, style, Feature, format } from 'openlayers';
 
 
 import { GeojsonService } from '../services/geojson.service';
@@ -12,13 +12,16 @@ import { GeojsonService } from '../services/geojson.service';
 })
 export class DrawToolComponent implements OnInit {
 
-    @Input() map: any;
+    @Input() map: Map;
 
     private vectorLayer: layer.Vector;
     private polygonDraw: interaction.Draw;
     private polygonModify: interaction.Modify;
     private polygonSelect: interaction.Select;
     private features: Feature[][] = [];
+    private properties: Object[];
+
+    private formatGeojson = new format.GeoJSON();
 
     private layerSource = new source.Vector();
     private style = new style.Style({
@@ -87,23 +90,83 @@ export class DrawToolComponent implements OnInit {
      * @param type 绘制类型
      */
     creat(type) {
-        const floorIndex = this.features.length;
-        const tempFeatures = this.layerSource.getFeatures();
-        if (type === 'floor' && tempFeatures.length === 0) {
-            console.log('请先创建Mall！');
-            return;
-        } else {
-            if (type === 'floor' && tempFeatures.length !== 1) {
-                // 如果是新建Floor，则把上次绘制的Floor放到临时缓存图层数组中
-                // 除去第一个要素（Mall的轮廓）之外的就是上一层Floor的要素
-                this.features[floorIndex] = tempFeatures.slice(1);
-                this.features[floorIndex].forEach((item) => {
-                    this.layerSource.removeFeature(item);
-                });
-            }
-        }
+        this.saveToCache(type);
         this.clearInteraction();
         this.polygonDraw.setActive(true);
+    }
+
+    private saveToCache(type?: string) {
+        const layerCurrentFeatures = this.layerSource.getFeatures();
+        if (type === 'floor' && layerCurrentFeatures.length > 0) {
+            const floorIndex = this.features.length;
+            // 如果当前绘制楼层为空，则不允许绘制新的楼层
+            if (type === 'floor' && layerCurrentFeatures.length !== 1) {
+                // 如果是新建Floor，则把上次绘制的Floor放到临时缓存图层数组中
+                // 除去第一个要素（Mall的轮廓）之外的就是上一层Floor的要素
+                this.features[floorIndex] = layerCurrentFeatures.slice(1);
+                for (let i = 0; i < this.features[floorIndex].length; i++) {
+                    const element = this.features[floorIndex][i];
+                    element.setProperties({ id: this.randomString(32), floor: floorIndex });
+                    this.layerSource.removeFeature(element);
+                }
+            }
+        } else {
+            console.log('请先创建Mall！');
+            return false;
+        }
+    }
+
+    // log st.
+    log() {
+        console.log(this.vectorLayer);
+        console.log(this.layerSource);
+        console.log('%c 当前图层中的要素：', 'color:green;font-weight:bold;');
+        console.log(this.vectorLayer.getSource().getFeatures());
+        // geometry 只有图形要素，没有属性，属性在feature上
+        this.vectorLayer.getSource().getFeatures()[0].setProperties({ type: 'mall', id: '1s3334' });
+        this.vectorLayer.getSource().getFeatures()[0].getGeometry().setProperties({ name: 'test', id: '1s3334' });
+        console.log('%c 当前缓存中的要素：', 'color:green;font-weight:bold;');
+        console.log(this.features);
+        console.log(this.formatGeojson.writeFeatures(this.features[0]));
+    }
+
+    draw(event: Event, type) {
+        event.stopPropagation();
+        this.clearInteraction();
+        this.polygonDraw.setActive(true);
+    }
+
+    edit(event: Event, type) {
+        event.stopPropagation();
+        this.clearInteraction();
+        this.polygonSelect.setActive(true);
+        this.polygonModify.setActive(true);
+    }
+
+    save() {
+        let features: Feature[] = [];
+        // 保存之前，把所有的floor都保存到临时缓存图层中,所以vectorLayer只有一个feature
+        this.saveToCache('floor');
+        // mall
+        features[0] = this.vectorLayer.getSource().getFeatures()[0];
+        features[0].setProperties({ type: 'mall', id: this.randomString(32) });
+        // floors
+        this.features.forEach((item) => {
+            features = features.concat(item);
+        });
+        const geojson = this.formatGeojson.writeFeatures(features);
+        console.log(geojson);
+
+        this.createAndDownloadFile('geo.json', geojson);
+    }
+
+    private createAndDownloadFile(fileName, content) {
+        const aTag = document.createElement('a');
+        const blob = new Blob([content]);
+        aTag.download = fileName;
+        aTag.href = URL.createObjectURL(blob);
+        aTag.click();
+        URL.revokeObjectURL(aTag.href);
     }
 
     private clearInteraction() {
@@ -112,34 +175,11 @@ export class DrawToolComponent implements OnInit {
         this.polygonDraw.setActive(false);
     }
 
-    // log st.
-    log() {
-        console.log(this.vectorLayer);
-        console.log(this.layerSource);
-        console.log('当前图层中的要素：');
-        console.log(this.vectorLayer.getSource().getFeatures());
-        console.log('当前缓存中的要素：');
-        console.log(this.features);
-    }
-
-    draw(event: Event, type) {
-        event.stopPropagation();
-        this.polygonDraw.setActive(true);
-    }
-
-
-    edit(event: Event, type) {
-        event.stopPropagation();
-        this.clearInteraction()
-        this.polygonSelect.setActive(true);
-        this.polygonModify.setActive(true);
-    }
-
     /**
      * 生成随机字符串
      * @param len 字符串长度
      */
-    private randomString(len): string {
+    private randomString(len?: number): string {
         len = len || 32;
         const $chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890';    /****默认去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1****/
         const maxPos = $chars.length;
@@ -150,7 +190,4 @@ export class DrawToolComponent implements OnInit {
         return pwd;
     }
 
-    save() {
-        this.geojsonService.toGeoJSON();
-    }
 }
