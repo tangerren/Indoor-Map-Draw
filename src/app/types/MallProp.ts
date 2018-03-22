b8083a0189e6b293b5603b3397474de9b5518fc2
WoShiSunWuKong <1026435687@qq.com>
2018-03-22 15:31:09 +0800

    mall and room properties model
    Signed-off-by: WoShiSunWuKong <1026435687@qq.com>

---
+++ src/app/types/MallProp.ts
@@ -0,0 +1,12 @@
+/**
+ * 楼宇属性
+ */
+export class MallProp {
+  id: string; // 唯一标识
+  name: string; // 楼宇名称
+  startFloor: number; // 开始楼层(地下几层)
+  endFloor: number; // 结束楼层
+  address: string; // 地址
+  pName: string; // 联系人姓名
+  pTel: string; // 联系人电话
+}
---
+++ src/app/types/RoomProp.ts
@@ -0,0 +1,11 @@
+/**
+ * 房间属性
+ */
+export class RoomProp {
+  id: string; // 唯一标识
+  name: string; // 名称
+  floor: number; // 所在楼层
+  type: string; // 房间类型
+  pName: string; // 联系人姓名
+  pTel: string; // 联系人电话
+}
