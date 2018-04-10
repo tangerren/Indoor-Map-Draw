5dff85ab5741be13d4f955ace25e926a77b16cb6
WoShiSunWuKong <1026435687@qq.com>
2018-04-10 22:15:23 +0800

    draw room reference to plans image
    Signed-off-by: WoShiSunWuKong <1026435687@qq.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, OnInit } from '@angular/core';
 
-import { Map, View, layer, source, proj } from 'openlayers';
+import { Map, View, layer, source, proj, style, Feature, geom, control } from 'openlayers';
 
 import { DrawToolComponent } from './draw-tool/draw-tool.component';
 
@@ -22,8 +22,39 @@ export class AppComponent implements OnInit {
     this.createMap();
   }
 
-
   private createMap() {
+
+    const imageStyle = new style.Style({
+      stroke: new style.Stroke({
+        color: 'blue',
+        width: 3
+      }),
+      image: new style.Icon({
+        src: 'https://woshisunwukong.github.io/image/indoor-map.jpg',
+        crossOrigin: 'Anonymous',
+        scale: 1,
+        rotateWithView: true
+      }),
+
+    });
+    const vecLayer = new layer.Vector({
+      style: imageStyle,
+      source: new source.Vector({
+        features: [new Feature({
+          geometry: geom.Polygon.fromExtent([11855662.310034806, 3452759.5557873524, 11855728.822132856, 3452823.3714489955])
+        })]
+      })
+    });
+
+    const imageLayer = new layer.Image({
+      source: new source.ImageStatic({
+        url: 'https://woshisunwukong.github.io/image/indoor-map.jpg',
+        imageExtent: [11855662.310034806, 3452759.5557873524, 11855728.822132856, 3452823.3714489955],
+        projection: 'EPSG:3857'
+      })
+    });
+
+
     const ele = this.elementRef.nativeElement.querySelector('#map');
     console.log(ele);
     this.map = new Map({
@@ -34,9 +65,17 @@ export class AppComponent implements OnInit {
         })
       ],
       view: new View({
-        center: proj.fromLonLat([105.41, 29.82]),
-        zoom: 4
-      })
+        center: proj.fromLonLat([106.50164388120174, 29.6043605183267]),
+        zoom: 20
+      }),
+      controls: [new control.MousePosition({
+        className: 'position-lable',
+        projection: 'EPSG:3857'
+      })]
     });
+
+    this.map.addLayer(vecLayer);
+    this.map.addLayer(imageLayer);
+
   }
 }
