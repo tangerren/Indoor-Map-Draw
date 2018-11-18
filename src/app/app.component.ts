9f546fd0454d685aed8cd6fab846d9e0eceac160
WoShiSunWuKong <1026435687@qq.com>
2018-11-18 12:22:22 +0800

    main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,7 @@ import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<router-outlet></router-outlet>',
 })
 
 
