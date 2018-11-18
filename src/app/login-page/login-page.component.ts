826a0d64d8a678b6b2382a3a4a5ea1ae559d366e
WoShiSunWuKong <1026435687@qq.com>
2018-11-18 12:19:12 +0800

    login page

---
+++ src/app/login-page/login-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
