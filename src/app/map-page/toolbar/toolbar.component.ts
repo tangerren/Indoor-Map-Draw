e236265fffa7923bca1dba25fec4cd59538f0a35
WoShiSunWuKong <1026435687@qq.com>
2018-11-23 11:10:25 +0800

    feature editing and drawing style

--- src/app/map-page/toolbar/toolbar.component.ts
+++ src/app/map-page/toolbar/toolbar.component.ts
@@ -5,6 +5,7 @@ import Polygon from 'ol/geom/Polygon';
 import Style from 'ol/style/Style';
 import Fill from 'ol/style/Fill';
 import Stroke from 'ol/style/Stroke';
+import Icon from 'ol/style/Icon';
 import CircleStyle from 'ol/style/Circle';
 import VerctorSource from 'ol/source/Vector';
 import VectorLayer from 'ol/layer/Vector';
@@ -22,145 +23,126 @@ import Snap from 'ol/interaction/Snap';
 export class ToolbarComponent implements OnInit {
 
   @Input() map: Map;
-  private polyLineDraw: Draw;
   private polygonDraw: Draw;
+  private polyLineDraw: Draw;
   private polygonSelect: Select;
   private polygonModify: Modify;
   private snap: Snap;
 
-  private vectorLayer: VectorLayer;
+  private pLayer: VectorLayer;
   private tempLayer: VectorLayer;
   private vectorSource = new VerctorSource();
   private tempSource = new VerctorSource();
 
-  private style = new Style({
+  private fillStyle = new Fill({
+    color: 'rgba(255, 255, 255, 0.2)'
+  });
+  private strokeStyle = new Stroke({
+    color: '#ffcc33',
+    width: 3,
+    lineJoin: 'bevel',
+    lineCap: 'square'
+  });
+  private strokeStyle_d = this.strokeStyle.clone();
+  private strokeStyle_e = this.strokeStyle.clone();
+  private strokeStyle_s = this.strokeStyle.clone();
+
+  private imageStyle = new CircleStyle({
+    radius: 7,
     fill: new Fill({
-      color: 'rgba(255, 255, 255, 0.2)'
-    }),
-    stroke: new Stroke({
-      color: '#ffcc33',
-      width: 3,
-      lineJoin: 'bevel',
-      lineDash: [10, 10, 10],
-      lineCap: 'square'
-    }),
-    image: new CircleStyle({
-      radius: 7,
-      fill: new Fill({
-        color: '#ffcc33'
-      })
+      color: '#FF0000'
     })
   });
+
 
   constructor() { }
 
   ngOnInit() {
+    this.strokeStyle_e.setLineDash([10, 10, 10]);
+    this.strokeStyle_s.setColor('#FF0000');
+
     // 创建绘图图层
-    this.vectorLayer = new VectorLayer({
+    this.pLayer = new VectorLayer({
       source: this.vectorSource,
-      style: this.style
+      style: new Style({
+        fill: this.fillStyle,
+        stroke: this.strokeStyle_d,
+        image: this.imageStyle
+      })
     });
-    this.map.addLayer(this.vectorLayer);
+    this.map.addLayer(this.pLayer);
 
     // 创建绘图图层
     this.tempLayer = new VectorLayer({
       source: this.tempSource,
-      style: this.style
+      style: new Style({
+        fill: this.fillStyle,
+        stroke: this.strokeStyle_s,
+        image: this.imageStyle
+      })
     });
     this.map.addLayer(this.tempLayer);
-
-    // 编辑交互
-    this.polygonModify = new Modify({
-      source: this.vectorSource
-    });
-    this.map.addInteraction(this.polygonModify);
-    this.polygonModify.setActive(false);
-
-    // 选中交互
-    this.polygonSelect = new Select({
-      multi: false,
-      // TODO: 设置编辑、选中时候的样式
-      // style: this.style
-    });
-    this.map.addInteraction(this.polygonSelect);
-    this.polygonSelect.setActive(false);
-
-    // 注册选中事件
-    this.polygonSelect.on('select', (e) => {
-      console.log(e);
-    });
   }
 
   // 编辑属性
   editProp(event) {
     event.stopPropagation();
     this.clearInteraction();
-    // TODO:设置编辑、选中时候的样式
-    this.polygonSelect.setActive(true);
+    this.addSelectInteractions('editProp');
   }
 
   // 编辑节点
   editVector(event) {
     event.stopPropagation();
     this.clearInteraction();
-    // TODO:设置编辑时候的样式
-    this.polygonModify.setActive(true);
+    this.pLayer.getStyle().setStroke(this.strokeStyle_e);
+    this.pLayer.changed();
+    // 编辑交互
+    this.polygonModify = new Modify({
+      source: this.vectorSource
+    });
+    this.map.addInteraction(this.polygonModify);
   }
 
   // 绘制多边形
   drawPolygon(event) {
     event.stopPropagation();
     this.clearInteraction();
-    this.addInteractions('Polygon');
+    this.addDrawInteractions('Polygon');
   }
 
   // 绘制矩形
   drawRec(event) {
     event.stopPropagation();
     this.clearInteraction();
-    this.polygonDraw = new Draw({
-      source: this.vectorSource,
-      type: 'Circle',
-      geometryFunction: createBox()
-    });
-    this.polygonDraw.on('drawend', (e: Draw.Event) => {
-      console.log('drawed');
-      console.log(this.vectorSource);
-    });
-    this.map.addInteraction(this.polygonDraw);
+    this.addDrawInteractions('Circle', createBox());
   }
   // 绘制圆
   drawCircle(event) {
     event.stopPropagation();
     this.clearInteraction();
-    this.addInteractions('Circle');
+    this.addDrawInteractions('Circle');
   }
   // 切割多边形
   splitPolygon(event) {
-    // 绘制线
-    this.polyLineDraw = new Draw({
-      source: this.tempSource,
-      type: 'LineString'
-    });
-    this.polyLineDraw.on('drawend', (e: Draw.Event) => {
-      console.log('splited');
-    });
-    this.map.addInteraction(this.polyLineDraw);
-    this.polyLineDraw.setActive(false);
+    event.stopPropagation();
+    this.clearInteraction();
+    this.addSelectInteractions('splitPolygon');
   }
   // 保存
   saveFloor(event) {
 
   }
 
-  addInteractions(type) {
+  // 添加绘制多边形交互
+  addDrawInteractions(type: string, geoFun?) {
     this.polygonDraw = new Draw({
       source: this.vectorSource,
-      type: type
+      type: type,
+      geometryFunction: geoFun
     });
     this.polygonDraw.on('drawend', (e: Draw.Event) => {
-      console.log('drawed');
-      console.log(this.vectorSource);
+      console.log('drawed' + this.vectorSource);
     });
     this.map.addInteraction(this.polygonDraw);
 
@@ -171,11 +153,72 @@ export class ToolbarComponent implements OnInit {
     this.map.addInteraction(this.snap);
   }
 
+  // 添加选中交互
+  addSelectInteractions(type: string) {
+    this.polygonSelect = new Select({
+      multi: false,
+    });
+    this.map.addInteraction(this.polygonSelect);
+    // 注册选中事件
+    this.polygonSelect.on('select', (e) => {
+      if (e.selected.length === 1) {
+        if (type === 'editProp') {
+          // TODO:弹出悬浮窗，编辑房间属性
+          console.log('弹出悬浮窗：' + e);
+        } else if (type === 'splitPolygon' && !this.polyLineDraw) {
+          this.drawLineToSpilt();
+          console.log('开始绘制线' + e);
+        }
+      }
+    });
+  }
+
+  // 移除交互
   private clearInteraction() {
     this.map.removeInteraction(this.polygonDraw);
     this.map.removeInteraction(this.snap);
-    this.polygonModify.setActive(false);
-    this.polygonSelect.setActive(false);
-    // this.polygonSelect.un('select', null);
+    this.map.removeInteraction(this.polygonSelect);
+    this.map.removeInteraction(this.polygonModify);
+
+    this.pLayer.getStyle().setStroke(this.strokeStyle);
+    this.pLayer.changed();
+
+    if (this.polygonSelect) {
+      this.polygonSelect.getFeatures().clear();
+    }
+  }
+
+  // 绘制切割线
+  drawLineToSpilt() {
+    this.polyLineDraw = new Draw({
+      source: this.tempSource,
+      type: 'LineString',
+      stopClick: true, // 为true时双击结束绘制，不会放大地图
+      style: new Style({ stroke: this.strokeStyle_s })
+    });
+    this.polyLineDraw.once('drawend', (e: Draw.Event) => {
+      this.split().then(() => {
+        // 要放到异步函数中去清空绘制的图层，因为在DRAWEND的时候，绘制的内容还未放到source中
+        this.tempSource.clear();
+        this.polygonSelect.getFeatures().clear();
+      });
+      this.map.removeInteraction(this.polyLineDraw);
+      this.polyLineDraw = null;
+    });
+    this.map.addInteraction(this.polyLineDraw);
+  }
+
+  // geoTool 切割geojson
+  split() {
+    // TODO:切割要素
+    console.log('splited');
+    return new Promise((resolve, reject) => {
+      console.log('my code'); // 你的代码
+      if (true) {
+        resolve(); // 将结果result传给resolve（）
+      } else {
+        reject(); // 如果你的代码执行没有成功或者出现了异常
+      }
+    });
   }
 }
